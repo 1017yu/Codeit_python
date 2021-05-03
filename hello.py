fa1661506d385eccbf394ab2c91e6f5a408b1b5e
kled <1017yu@daum.net>
2021-05-03 15:58:17 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ hello("Kled")
 def print_sum(a, b, c):    # 여러 개의 parameter를 통한 sum function
     print(a + b + c)
 
-print_sum(7, 3, 10)
+print_sum(7, 3, 111)
 
 ##################################################################################################
 
