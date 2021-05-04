38f74ce21dacaf79c69bd5af30fbc4416e3af63a
kled <1017yu@daum.net>
2021-05-04 21:48:51 +0900

    Update number_operations.py

--- number_operations.py
+++ number_operations.py
@@ -1,7 +1,9 @@
-#floor division (버림 나눗셈)
+# floor division (버림 나눗셈)
 print(8.0 // 3) # 몫만 계산
 
-#round (반올림)
+# round (반올림)
 print(round(3.1415))
 
-print(round(3.1415, 2)) #소수점 둘째자리까지
+print(round(3.1415, 2))  # 소수점 둘째자리까지
+
+print(10 / (10 % 6))  # 10 % 6 = 4 (나머지)
