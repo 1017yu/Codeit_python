8d7af073ad2d35e85aa9866b3419e4fdbdf2100d
kled <1017yu@daum.net>
2021-05-04 21:35:14 +0900

    Add string_data_type.py

---
+++ string_data_type.py
@@ -0,0 +1,10 @@
+print("코드잇") # String 출력
+print("유재석")
+
+# print('I'm exticted to learn Python!'')   잘못된 예
+print("I'm exticted to learn Python!")
+print('I\'m exticted to learn Python!')
+
+print("Hello" * 3) #문자열 3회 나열하기
+
+print("3" + "5") #정수가 아닌 문자열 3과 5를 연결
