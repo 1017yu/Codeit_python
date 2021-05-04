5ef893415792c39fe7470d0cbc5f038626601aac
kled <1017yu@daum.net>
2021-05-04 22:14:16 +0900

    Add type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,15 @@
+print(int(3.8))     # 정수 3 출력
+
+print(float(3))     # 소수 3.0 출력
+
+print(int("2") + int("5"))      # 문자열을 정수로 출력하여 계산)
+
+print(float("1.1") + float("2.5"))
+
+print(str(2) + str(5))      # 정수를 문자열로 출력
+
+age = 7
+# print("제 나이는" + age + "살입니다") 잘못된 표현
+print("제 나이는 " + str(age) + "살 입니다")
+
+
