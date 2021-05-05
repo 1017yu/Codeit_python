a60747ef6646e3f98b9a51eb825cfc6b7afeb34d
kled <1017yu@daum.net>
2021-05-05 15:35:58 +0900

    Add type_function.py

---
+++ type_function.py
@@ -0,0 +1,17 @@
+print(type(3))  # integer
+
+print(type(3.0))  # float
+
+print(type("3"))  # string
+
+print(type("True"))  # string
+
+print(type(True))  # boolean
+
+def hello():
+    print("Hello World!")
+
+
+print(type(hello))  # function
+
+print(type(print))  # builtin_function
