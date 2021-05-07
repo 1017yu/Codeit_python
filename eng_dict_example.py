e193d33355adc9fe6a404c13265c63700770811d
kled <1017yu@daum.net>
2021-05-07 17:09:52 +0900

    Add eng_dict_example.py

---
+++ eng_dict_example.py
@@ -0,0 +1,14 @@
+# 1. 단어장 만들기
+vocab = {
+    'sanitizer': '살균제',  # key: value 쌍
+    'ambition': '야망',
+    'conscience': '양심',
+    'civilization': '문명'
+}
+print(vocab)
+
+
+# 2. 새로운 단어들 추가하기
+vocab['privilege'] = '특권'  # vocab[key] = value의 형태
+vocab['principle'] = '원칙'
+print(vocab)
