7212a0bc3ee8c7671104c54b676e778b3171295d
kled <1017yu@daum.net>
2021-05-08 12:44:52 +0900

    Add security_number.py

--- security_number.py
+++ security_number.py
@@ -14,6 +14,9 @@ def mask_security_number(security_number):
     return total_str  # 문자열로 재변환한 total_str을 리턴
 
 
+#  사실 간단한 방법은 mask_security_number 함수에 return security_number[:-4] + '****' 로 슬라이싱하면 된다
+
+
 # 테스트
 print(mask_security_number("880720-1234567"))
 print(mask_security_number("8807201234567"))
