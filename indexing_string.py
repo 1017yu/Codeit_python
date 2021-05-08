1d18860cbe086775c942b1b24ba9d1efd0a54026
kled <1017yu@daum.net>
2021-05-08 11:37:42 +0900

    Add indexing_string.py

---
+++ indexing_string.py
@@ -0,0 +1,46 @@
+# 알파벳 리스트의 인덱싱
+alphabets_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+print(alphabets_list[0])  # A
+print(alphabets_list[1])  # B
+print(alphabets_list[4])  # E
+print(alphabets_list[-1])  # J
+
+# 알파벳 문자열의 인덱싱
+alphabets_string = 'ABCDEFGHIJ'
+print(alphabets_string[0])  # A
+print(alphabets_string[1])  # B
+print(alphabets_string[4])  # E
+print(alphabets_string[-1])  # J
+
+
+# 알파벳 리스트의 반복문
+alphabets_list = ['C', 'O', 'D', 'E', 'I', 'T']
+for alphabet in alphabets_list:  # alphabets_list 에 있는 알파벳을 alphabet 이라는 변수에 지정
+    print(alphabet)
+
+
+# 알파벳 문자열의 반복문
+alphabets_string = 'CODEIT'
+for alphabet in alphabets_string:  # alphabets_string 에 있는 알파벳을 alphabet 이라는 변수에 지정
+    print(alphabet)
+
+
+# 알파벳 리스트의 슬라이싱
+alphabets_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+print(alphabets_list[0:5])  # 'A'부터 'E'까지
+print(alphabets_list[4:])  # 'E'부터 끝까지
+print(alphabets_list[:4])  # 처움부터 'D'까지
+
+# 알파벳 문자열의 슬라이싱
+alphabets_string = 'ABCDEFGHIJ'
+print(alphabets_string[0:5])  # A부터 E까지
+print(alphabets_string[4:])
+print(alphabets_string[:4])
+
+
+# len 함수의 활용
+# 리스트의 길이 재기
+print(len(['H', 'E', 'L', 'L', 'O']))
+
+# 문자열의 길이 재기
+print(len("Hello, world!"))
